258f78f8395a7f9c3dc07f5a3b9c84d5e62f1f25
ImHamba <ImHamba@users.noreply.github.com>
2024-05-26 12:34:44 +0000

    deploy: 8602bf9f707a43e09467b85686b2c0c5036c0083

---
+++ _app/immutable/chunks/paths.26f5abae.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_15uympj)==null?void 0:s.base)??"/timeless-jewels";var e;const a=((e=globalThis.__sveltekit_15uympj)==null?void 0:e.assets)??t;export{a,t as b};
